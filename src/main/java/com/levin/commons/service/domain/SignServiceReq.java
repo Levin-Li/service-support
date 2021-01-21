95b0b2d14b2e8d47f894cd29544fed6908cc03a9
llw <vince2li@gmail.com>
2021-01-21 17:33:04 +0800

    增加忽略参数

--- src/main/java/com/levin/commons/service/domain/SignServiceReq.java
+++ src/main/java/com/levin/commons/service/domain/SignServiceReq.java
@@ -18,21 +18,27 @@ public abstract class SignServiceReq
         implements ServiceReq {
 
     @Schema(description = "应用标识")
+    @Ignore
     String appId;
 
     @Schema(description = "应用密钥")
+    @Ignore
     String appSecret;
 
     @Schema(description = "一次使用临时字符串")
+    @Ignore
     String nonceStr;
 
     @Schema(description = "时间串")
+    @Ignore
     String timeStamp;
 
     @Schema(description = "渠道编码")
+    @Ignore
     String channelCode;
 
     @Schema(description = "签名串")
+    @Ignore
     String sign;
 
     @Override
