429e1ada8e2fa1ccecb5ca7783fecf928eab95eb
llw <vince2li@gmail.com>
2021-10-27 15:24:23 +0800

    优化被抛弃的类

--- src/main/java/com/levin/commons/service/domain/Desc.java
+++ src/main/java/com/levin/commons/service/domain/Desc.java
@@ -1,16 +1,13 @@
 package com.levin.commons.service.domain;
 
 
-import com.levin.commons.enums.DescOperable;
-import com.levin.commons.enums.DescType;
-
 import java.lang.annotation.*;
 
-/*
+/**
  * 类，方法，参数，字段的描述注解
  *
  * @author Laishr
- * @version 1.0,2015/11/06
+ * @version 1.0, 2015/11/06
  * Copyright (c) 2015.
  */
 @Target({ElementType.PACKAGE, ElementType.TYPE,
@@ -72,49 +69,10 @@ public @interface Desc {
      */
     boolean toDoc() default true;
 
-    /**
-     * 数据类型
-     *
-     * @return DescType
-     */
-    DescType type() default DescType.DEFAULT;
-
-    /**
-     * 可操作定义
-     *
-     * @return
-     */
-    DescOperable[] operable() default {};
-
-    /**
-     * 数据绑定配置
-     *
-     * @return DataBind
-     */
-    DataBind bind() default @DataBind(value = "NONE");
-
-    String[] params() default {};
-
-    /**
-     * 检查非法字符
-     * @return
-     */
-    boolean checkIllegal() default false;
-
-    /**
-     * 关联到字段
-     * @return
-     */
-    String cascadeTo() default "";
-
-    /**
-     * 关联到字段的值匹配时生效
-     * @return
-     */
-    String cascadeEq() default "true";
 
     /**
      * 隐藏的
+     *
      * @return
      */
     boolean hide() default false;
