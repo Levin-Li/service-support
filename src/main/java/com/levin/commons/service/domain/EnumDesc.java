3215f39d76811a4d460419c2112b47574ce6323b
llw <vince2li@gmail.com>
2021-11-25 11:59:43 +0800

    优化权限模块9

--- src/main/java/com/levin/commons/service/domain/EnumDesc.java
+++ src/main/java/com/levin/commons/service/domain/EnumDesc.java
@@ -3,57 +3,110 @@ package com.levin.commons.service.domain;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- *
+ * 获取 Schema 注解的名称描述
  */
 public interface EnumDesc {
 
-    EnumDesc helper = new EnumDesc() {
-        @Override
-        public String getDesc() {
-            throw new RuntimeException("current object is not an enum object");
-        }
-    };
-
     Map<Enum, String> cacheNames = new ConcurrentHashMap<>();
 
+    /**
+     * 已枚举索引为key 获取枚举描述
+     * 枚举索引 从 0 开始
+     *
+     * @param type
+     * @return
+     */
+    static Map<Integer, String> getDescMapByIndex(Class<? extends Enum> type) {
 
+        if (!type.isEnum()) {
+            throw new IllegalArgumentException(type + "  is not a enum type");
+        }
+
+        Map<Integer, String> desc = new LinkedHashMap<>();
+
+        Arrays.stream(type.getEnumConstants())
+                .parallel().forEachOrdered(anEnum -> {
+                    desc.put(anEnum.ordinal(), (anEnum instanceof EnumDesc) ? ((EnumDesc) anEnum).getDesc() : getDesc(anEnum));
+                }
+        );
+
+        return desc;
+    }
+
+    /**
+     * 已枚举名称为key 获取枚举描述
+     *
+     * @param type
+     * @return
+     */
+    static Map<String, String> getDescMapByName(Class<? extends Enum> type) {
+
+        if (!type.isEnum()) {
+            throw new IllegalArgumentException(type + "  is not a enum type");
+        }
+
+        Map<String, String> desc = new LinkedHashMap<>();
+
+        Arrays.stream(type.getEnumConstants())
+                .parallel().forEachOrdered(anEnum -> {
+                    desc.put(anEnum.name(), (anEnum instanceof EnumDesc) ? ((EnumDesc) anEnum).getDesc() : getDesc(anEnum));
+                }
+        );
+
+        return desc;
+    }
+
+    /**
+     * 获取描述
+     * 该方法可以覆盖
+     *
+     * @return
+     */
     default String getDesc() {
         return getDesc((Enum<?>) this);
     }
 
-    default String getDesc(Enum<?> enumValue) {
+    /**
+     * 获取 Schema 注解描述
+     *
+     * @param anEnum
+     * @return
+     */
+    static String getDesc(Enum<?> anEnum) {
 
-        if (enumValue == null) {
+        if (anEnum == null) {
             return null;
         }
 
-        String info = cacheNames.get(enumValue);
+        String info = cacheNames.get(anEnum);
 
         if (info != null) {
             return info;
         }
 
-        synchronized (enumValue) {
+        synchronized (anEnum) {
 
             Schema annotation = null;
 
             try {
-                annotation = enumValue.getClass().getField(enumValue.name()).getAnnotation(Schema.class);
+                annotation = anEnum.getDeclaringClass().getField(anEnum.name()).getAnnotation(Schema.class);
             } catch (NoSuchFieldException e) {
                 throw new RuntimeException(e);
             }
 
-            info = annotation != null ? annotation.description() : enumValue.toString();
+            info = annotation != null ? annotation.description() : anEnum.toString();
 
-            if (info.trim().length() == 0) {
-                info = enumValue.name();
+            if (info == null || info.trim().length() == 0) {
+                info = anEnum.name();
             }
 
-            cacheNames.put(enumValue, info);
+            cacheNames.put(anEnum, info);
         }
 
         return info;
