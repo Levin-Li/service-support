f89e42842bacaad599d694a7e09a95d8e25bfd3e
Levin-Li <vince2li@gmail.com>
2023-02-19 16:46:35 +0800

    add

---
+++ src/main/java/com/levin/commons/service/exception/UnauthorizedException.java
@@ -0,0 +1,25 @@
+package com.levin.commons.service.exception;
+
+/**
+ * 未认证异常
+ */
+public class UnauthorizedException
+        extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public UnauthorizedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UnauthorizedException(Throwable cause) {
+        super(cause);
+    }
+
+    protected UnauthorizedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
