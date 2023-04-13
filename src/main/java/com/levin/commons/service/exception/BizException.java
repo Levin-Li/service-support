880dc48f8adea0b393c00f74a59c4cd12506825b
Levin-Li <vince2li@gmail.com>
2023-04-13 22:36:42 +0800

    优化用户

---
+++ src/main/java/com/levin/commons/service/exception/BizException.java
@@ -0,0 +1,50 @@
+package com.levin.commons.service.exception;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+/**
+ * 服务异常，服务方法都应该声明异常
+ * <p/>
+ * <p/>
+ * Created by echo on 2015/11/22.
+ *
+ * @author lilw
+ */
+@Schema(title = "义务异常类")
+public class BizException
+        extends RuntimeException {
+
+    private static final long serialVersionUID = -677894470754649710L;
+
+    //异常编码
+    @Schema(title = "异常编码")
+    String code;
+
+    @Schema(title = "友好提示信息", description = "可以直接展示给用户看")
+    String friendlyTips;
+
+    public BizException(String code, String message) {
+        this(code, message, null);
+    }
+
+
+    public BizException(String message, Throwable cause, String code, String friendlyTips) {
+        super(message, cause);
+        this.code = code;
+        this.friendlyTips = friendlyTips;
+    }
+
+    public BizException(String code, String message, Throwable cause) {
+        super(message, cause);
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+
+    public String getFriendlyTips() {
+        return friendlyTips;
+    }
+}
