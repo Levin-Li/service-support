7455daacadff6071040715845b6985b14d4adc5e
Levin-Li <vince2li@gmail.com>
2023-06-09 11:10:18 +0800

    优化类生成

--- src/main/java/com/levin/commons/processor/JpaEntityClassProcessor.java
+++ src/main/java/com/levin/commons/processor/JpaEntityClassProcessor.java
@@ -225,9 +225,12 @@ public class JpaEntityClassProcessor extends AbstractProcessor {
 
             if (typeElement.getAnnotation(MappedSuperclass.class) != null
                     || typeElement.getAnnotation(Entity.class) != null) {
+
                 codeBlock.append("    String ALIAS   = \"").append(getAlias(simpleClassName)).append("\"; // 别名1 \n\n");
                 codeBlock.append("    String ALIAS_2 = \"").append(getAlias(simpleClassName) + "_2").append("\"; // 别名2 \n\n");
                 codeBlock.append("    String ALIAS_3 = \"").append(getAlias(simpleClassName) + "_3").append("\"; // 别名3 \n\n");
+
+                codeBlock.append("    String F_ALIAS_P = \"").append("F$:").append("\" + ALIAS + \".\"; //  字段别名前缀 \n\n");
             }
 
             processAnnotation(typeElement, simpleClassName, codeBlock);
