51f4997c297a3688cc6478bde19c56417158b39d
Levin-Li <vince2li@gmail.com>
2023-07-15 23:14:47 +0800

    优化注入异常

--- src/main/java/com/levin/commons/format/DefaultDateFormat.java
+++ src/main/java/com/levin/commons/format/DefaultDateFormat.java
@@ -1,26 +1,64 @@
 package com.levin.commons.format;
 
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
-import com.fasterxml.jackson.databind.util.StdDateFormat;
+import cn.hutool.core.util.StrUtil;
 
-import java.text.ParseException;
+import java.text.DateFormat;
+import java.text.FieldPosition;
 import java.text.ParsePosition;
 import java.util.Date;
 
 /**
  * 日期格式化类
  * <p>
+ * 自动解析多种样式
+ *
+ * <p>
  * 调用hutool解析类，方便 json参数上传转换
  */
-public class DefaultDateFormat extends StdDateFormat {
-    @Override
-    protected Date _parseDate(String dateStr, ParsePosition pos) throws ParseException {
 
-        Date date = super._parseDate(dateStr, pos);
+public class DefaultDateFormat extends DateFormat {
 
-        if (date == null) {
-            return DateUtil.parse(dateStr).toJdkDate();
-        }
-        return null;
+    final String format;
+
+    public DefaultDateFormat() {
+        this(null);
     }
+
+    public DefaultDateFormat(String format) {
+
+        if (StrUtil.isBlank(format)) {
+            format = DatePattern.NORM_DATETIME_PATTERN;
+        }
+
+        this.format = format;
+    }
+
+    @Override
+    public StringBuffer format(Date date, StringBuffer toAppendTo, FieldPosition fieldPosition) {
+
+        toAppendTo.append(DateUtil.format(date, format));
+
+        return toAppendTo;
+    }
+
+    @Override
+    public Date parse(String source, ParsePosition pos) {
+
+        Date result = null;
+
+        DateTime parse = DateUtil.parse(source);
+
+        if (parse != null) {
+            result = parse.toJdkDate();
+        }
+
+        //非常重要，标记为解析完成
+        pos.setIndex(source.length());
+
+        return result;
+    }
+
 }
