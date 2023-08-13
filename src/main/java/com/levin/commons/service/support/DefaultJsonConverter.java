58c214a051b9759a25e2c1da31563ea167a5cf99
Levin-Li <vince2li@gmail.com>
2023-08-13 14:32:24 +0800

    优化Json转换器

--- src/main/java/com/levin/commons/service/support/DefaultJsonConverter.java
+++ src/main/java/com/levin/commons/service/support/DefaultJsonConverter.java
@@ -9,6 +9,8 @@ import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.util.StringUtils;
 
+import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.Set;
 
 /**
@@ -55,7 +57,48 @@ public class DefaultJsonConverter implements GenericConverter {
             ResolvableType rt = targetType.getResolvableType();
             Assert.isTrue(!rt.hasUnresolvableGenerics(), "目标类型中有未识别的泛型：" + rt);
 
-            return gson.fromJson(source.toString(), rt.hasGenerics() ? rt.getType() : rt.resolve());
+            String json = source.toString();
+
+            //如果没有内容
+            if (!StringUtils.hasText(json)) {
+                return null;
+            }
+
+            Type type = rt.hasGenerics() ? rt.getType() : rt.resolve();
+            Assert.notNull(type, "Json转换时目标类型未指定");
+
+            //以下代码主要为优化兼容性
+            //去除前后的空格，回车的字符
+            json = StringUtils.trimWhitespace(json);
+
+            //循环删除最后一个逗号
+            while (json.charAt(json.length() - 1) == ',') {
+                json = StringUtils.trimWhitespace(json.substring(0, json.length() - 1));
+            }
+
+            //自动补全JSON格式塔
+
+            //数组，集合
+            if (rt.isArray()
+                    || (rt.resolve() != null && Collection.class.isAssignableFrom(rt.resolve()))) {
+
+                if (json.charAt(0) != '[') {
+                    json = "[" + json;
+                }
+                if (json.charAt(json.length() - 1) != ']') {
+                    json = json + "]";
+                }
+            } else {
+                if (json.charAt(0) != '{') {
+                    json = "{" + json;
+                }
+                if (json.charAt(json.length() - 1) != '}') {
+                    json = json + "}";
+                }
+            }
+
+
+            return gson.fromJson(json, type);
         }
 
         return source;
