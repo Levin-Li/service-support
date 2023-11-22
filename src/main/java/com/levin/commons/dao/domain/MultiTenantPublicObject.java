e5130bbaddf146e5dbb5076b2c7591012b6f4100
lilw <vince2li@gmail.com>
2023-11-22 11:00:02 +0800

    优化变量解析器管理类

---
+++ src/main/java/com/levin/commons/dao/domain/MultiTenantPublicObject.java
@@ -0,0 +1,13 @@
+package com.levin.commons.dao.domain;
+
+
+/**
+ * 多租户对象
+ * 允许租户ID为空，做系统的公共数据
+ *
+ * @Author levin li
+ * @Since 2.2.23
+ */
+public interface MultiTenantPublicObject extends MultiTenantObject {
+
+}
