b1d8c10637bc353502ae9b627df91f2b6575b00f
lilw <vince2li@gmail.com>
2024-04-01 11:22:03 +0800

    兼容旧版本

---
+++ src/main/java/com/levin/commons/utils/CpuUtils.java
@@ -0,0 +1,91 @@
+package com.levin.commons.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+
+
+@Slf4j
+public class CpuUtils {
+
+    private static long lastCollectTime = -1;
+    private static int lastCollectValue = -1;
+
+    private static final CentralProcessor processor = new SystemInfo().getHardware().getProcessor();
+
+    private static StatHelper statHelper = new StatHelper();
+
+    static {
+
+        new Thread(() -> {
+
+            while (!Thread.currentThread().isInterrupted()) {
+
+                collectCpuLoadRate(300);
+
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * 采集CPU利用率
+     *
+     * @return
+     */
+    public static int collectCpuLoadRate(long collectInterval) {
+
+        //最小不能低于100毫秒
+        if (collectInterval < 100) {
+            collectInterval = 100L;
+        }
+
+        //超过1秒
+        if (lastCollectTime < 1 || (System.currentTimeMillis() - lastCollectTime) > collectInterval) {
+
+            lastCollectTime = System.currentTimeMillis();
+
+            // 获取系统范围的cpu负载技计数
+//            long[] prevTicks = processor.getSystemCpuLoadTicks();
+
+            try {
+                Thread.sleep(collectInterval);
+            } catch (InterruptedException e) {
+                return lastCollectValue;
+            }
+
+            //  long[] ticks = processor.getSystemCpuLoadTicks();
+
+            lastCollectValue = (int) (processor.getSystemCpuLoadBetweenTicks() * 100);
+        }
+
+        return lastCollectValue;
+    }
+
+
+    public static int getCpuLoad() {
+        return lastCollectValue;
+    }
+
+    public static int sleepIfCpuLoadOverThreshold(int cpuThreshold, long sleepMs) {
+
+        statHelper.onAlarm(15, 50, 0
+                , (growthRatio, lastRatio) -> log.warn("*** CPU负载 {} > {}，处理速率：{}，环比变化率：{}", getCpuLoad(), cpuThreshold, lastRatio, growthRatio));
+
+        if (getCpuLoad() >= cpuThreshold) {
+            try {
+                Thread.sleep(sleepMs);
+
+            } catch (InterruptedException e) {
+                return getCpuLoad();
+            }
+        }
+
+        return getCpuLoad();
+    }
+
+}
