421e6bf0e9bee7790034bd49102e9a8114489e0f
lilw <vince2li@gmail.com>
2024-04-07 00:23:58 +0800

    兼容旧版本

--- src/main/java/com/levin/commons/dao/domain/SelfOverridableObject.java
+++ src/main/java/com/levin/commons/dao/domain/SelfOverridableObject.java
@@ -1,6 +1,6 @@
 package com.levin.commons.dao.domain;
 
-import java.util.List;
+import java.lang.annotation.*;
 
 /**
  * 可自替换的对象
@@ -11,8 +11,11 @@ import java.util.List;
  * <p>
  * Override
  */
-@FunctionalInterface
-public interface SelfOverridableObject {
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface SelfOverridableObject {
 
     /**
      * 方法返回一个字段名列表，字段名对应的值组合成一个标识，然后用标识进行相等比较，用自己直接拥有的数据将替换公共数据。
@@ -21,6 +24,14 @@ public interface SelfOverridableObject {
      *
      * @return 返回字段名列表
      */
-    List<String> overrideColumnNames();
+    String[] overrideColumnNames();
+
+    /**
+     * 返回备注信息
+     *
+     * @return 返回字符串类型的备注信息
+     * @default 默认为空字符串
+     */
+    String remark() default "";
 
 }
