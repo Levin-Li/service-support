7f7856acacd5412b54b6f60b9995b588598dfc59
lilw <vince2li@gmail.com>
2025-01-17 14:43:23 +0800

    优化顺序

--- src/main/java/com/levin/commons/utils/DataMaskingUtils.java
+++ src/main/java/com/levin/commons/utils/DataMaskingUtils.java
@@ -2,12 +2,7 @@ package com.levin.commons.utils;
 
 import cn.hutool.core.util.StrUtil;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public abstract class DataMaskingUtils {
-
-    private static final int LZW_MAX_TABLE_SIZE = 4096;
 
     /**
      * @return
@@ -101,115 +96,6 @@ public abstract class DataMaskingUtils {
         }
 
         return result.toString();
-    }
-
-
-    /**
-     * LZW压缩（Lempel-Ziv-Welch Compression）
-     *
-     * @param input
-     * @return
-     */ // LZW压缩方法
-    public static String compress(String input) {
-        Map<String, Integer> dictionary = new HashMap<>();
-        int dictSize = 256;
-        for (int i = 0; i < 256; i++) {
-            dictionary.put(String.valueOf((char) i), i);
-        }
-
-        StringBuilder w = new StringBuilder();
-        StringBuilder result = new StringBuilder();
-        for (char c : input.toCharArray()) {
-            String wc = w.toString() + c;
-            if (dictionary.containsKey(wc)) {
-                w = new StringBuilder(wc);
-            } else {
-                result.append(dictionary.get(w.toString()));
-                dictionary.put(wc, dictSize++);
-                w = new StringBuilder(String.valueOf(c));
-            }
-        }
-
-        if (w.length() > 0) {
-            result.append(dictionary.get(w.toString()));
-        }
-
-        return result.toString();
-    }
-
-    // LZW解压缩方法
-    public static String decompress(String compressed) {
-        Map<Integer, String> dictionary = new HashMap<>();
-        int dictSize = 256;
-        for (int i = 0; i < 256; i++) {
-            dictionary.put(i, String.valueOf((char) i));
-        }
-
-        String[] codes = compressed.split("(?<=\\G.{1})");
-        StringBuilder w = new StringBuilder(dictionary.get(Integer.parseInt(codes[0])));
-        StringBuilder result = new StringBuilder(w);
-
-        for (int i = 1; i < codes.length; i++) {
-            int current = Integer.parseInt(codes[i]);
-            String entry;
-            if (dictionary.containsKey(current)) {
-                entry = dictionary.get(current);
-            } else if (current == dictSize) {
-                entry = w.toString() + w.charAt(0);
-            } else {
-                throw new IllegalArgumentException("Bad compressed k: " + current);
-            }
-
-            result.append(entry);
-
-            dictionary.put(dictSize++, w.toString() + entry.charAt(0));
-            w = new StringBuilder(entry);
-        }
-
-        return result.toString();
-    }
-
-
-    /**
-     * 运行长度编码（Run-Length Encoding, RLE）
-     * RLE是一种简单的压缩算法，通过将连续的相同字符替换为一个字符和其出现次数来减少数据长度。适用于包含大量重复字符的文件，如简单位图、ASCII文本文件等
-     *
-     * @param input
-     * @return
-     */
-    public static String rleCompress(String input) {
-
-        StringBuilder compressed = new StringBuilder();
-
-        int count = 1;
-        for (int i = 1; i < input.length(); i++) {
-            if (input.charAt(i) == input.charAt(i - 1)) {
-                count++;
-            } else {
-                compressed.append(input.charAt(i - 1)).append(count);
-                count = 1;
-            }
-        }
-
-        compressed.append(input.charAt(input.length() - 1)).append(count);
-
-        return compressed.toString();
-
-    }
-
-    public static String rleDecompress(String input) {
-
-        StringBuilder decompressed = new StringBuilder();
-
-        for (int i = 0; i < input.length(); i += 2) {
-            char character = input.charAt(i);
-            int count = Integer.parseInt(String.valueOf(input.charAt(i + 1)));
-            for (int j = 0; j < count; j++) {
-                decompressed.append(character);
-            }
-        }
-
-        return decompressed.toString();
     }
 
 }
