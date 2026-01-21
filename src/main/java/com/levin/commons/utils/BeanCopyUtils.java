2d79192e6486116d57111b3ba407ec8a7911abe5
lilw <vince2li@gmail.com>
2026-01-21 13:00:04 +0800

    优化新版rbac

---
+++ src/main/java/com/levin/commons/utils/BeanCopyUtils.java
@@ -0,0 +1,96 @@
+package com.levin.commons.utils;
+
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.cglib.beans.BeanCopier;
+import org.springframework.cglib.core.Converter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author lilw
+ */
+public abstract class BeanCopyUtils {
+
+    // 缓存 BeanCopier 实例（避免重复生成字节码，核心性能优化）
+    private static final Map<String, BeanCopier> copierCache = new ConcurrentHashMap<>();
+
+    public static BeanCopier getBeanCopier(Class<?> sourceClass, Class<?> targetClass, boolean useConverter) {
+
+        final String cacheKey = sourceClass.getName() + "_" + targetClass.getName() + "_" + useConverter;
+
+        BeanCopier beanCopier = copierCache.get(cacheKey);
+
+        // 双重检查锁，避免并发创建
+        if (beanCopier == null) {
+            synchronized (copierCache) {
+
+                beanCopier = copierCache.get(cacheKey);
+
+                //如果还是为空，则创建
+                if (beanCopier == null) {
+                    beanCopier = BeanCopier.create(sourceClass, targetClass, useConverter);
+                    copierCache.put(cacheKey, beanCopier);
+                }
+            }
+        }
+
+        return beanCopier;
+    }
+
+    /**
+     * 获取/缓存 BeanCopier 实例（核心：按 源类+目标类+是否带转换器 缓存）
+     */
+    public static BeanCopier getBeanCopier(Class<?> sourceClass, Class<?> targetClass) {
+        return getBeanCopier(sourceClass, targetClass, false);
+    }
+
+
+    public static <T> T copyProperties(Object sourceBean, Class<T> targetClass) {
+
+        BeanCopier beanCopier = getBeanCopier(sourceBean.getClass(), targetClass);
+
+        T targetBean = BeanUtils.instantiateClass(targetClass);
+
+        beanCopier.copy(sourceBean, targetBean, null);
+
+        return targetBean;
+    }
+
+    public static <T> T copyProperties(Object sourceBean, T targetBean) {
+        return copyProperties(sourceBean, targetBean, null, null);
+    }
+
+    public static <T> T copyProperties(Object sourceBean, Class<T> targetClass, Converter converter) {
+        return copyProperties(sourceBean, BeanUtils.instantiateClass(targetClass), targetClass, converter);
+    }
+
+    /**
+     * 拷贝属性
+     *
+     * @param sourceBean
+     * @param targetBean
+     * @param targetClass
+     * @param converter   转换器, 比如 时间转换器, String ->Integer 等
+     * @param <T>
+     * @return
+     */
+    public static <T> T copyProperties(Object sourceBean, T targetBean, Class<T> targetClass, Converter converter) {
+
+        if (targetBean == null || sourceBean == null) {
+            return targetBean;
+        }
+
+        if (targetClass == null) {
+            targetClass = (Class<T>) targetBean.getClass();
+        }
+
+        BeanCopier beanCopier = getBeanCopier(sourceBean.getClass(), targetClass, converter != null);
+
+        beanCopier.copy(sourceBean, targetBean, converter);
+
+        return targetBean;
+    }
+
+}
