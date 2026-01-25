73cfdcbf70ff4c86e35368603785619ced12d922
lilw <vince2li@gmail.com>
2026-01-25 22:16:12 +0800

    优化新版rbac

--- src/main/java/com/levin/commons/dao/domain/SelfAuditableObject.java
+++ src/main/java/com/levin/commons/dao/domain/SelfAuditableObject.java
@@ -1,7 +1,5 @@
 package com.levin.commons.dao.domain;
 
-
-import cn.hutool.core.lang.Assert;
 import com.levin.commons.service.domain.Identifiable;
 import io.swagger.v3.oas.annotations.Operation;
 
@@ -15,35 +13,37 @@ import java.util.Date;
  */
 public interface SelfAuditableObject {
 
-    @Operation(summary = "自审", description = "")
-    default <E extends SelfAuditableObject> E selfAudit() {
+    @Operation(summary = "自审并返回错误信息", description = "没有错误,则放回null")
+    default String selfAudit() {
 
         if (this instanceof Identifiable) {
-
             // id 不能为空, 不能为空字符串
             Serializable objectTempAuditId = ((Identifiable) this).getId();
 
-            Assert.isTrue(objectTempAuditId != null
-                    && (!(objectTempAuditId instanceof CharSequence) || !((CharSequence) objectTempAuditId).toString().trim().isEmpty()), "object id is required");
+            if (objectTempAuditId == null
+                    || (objectTempAuditId instanceof CharSequence && ((CharSequence) objectTempAuditId).toString().trim().isEmpty())) {
+                return "object id is required";
+            }
         }
 
-        if (this instanceof EnableObject) {
+        if (this instanceof EnableObject && !((EnableObject) this).isEnable()) {
             // 不能是未启用的
-            Assert.isTrue(((EnableObject) this).isEnable(), "object disable");
+            return "object is not enable";
         }
 
-        if (this instanceof LogicDeletableObject) {
+        if (this instanceof LogicDeletableObject && ((LogicDeletableObject) this).isDeleted()) {
             // 不能是逻辑删除的
-            Assert.isTrue(!((LogicDeletableObject) this).isDeleted(), "object logic deleted");
+            return ("object logic deleted");
         }
 
-        if (this instanceof ExpiredObject) {
-            // 不能是过期的
-            Assert.isTrue(((ExpiredObject) this).getExpiredTime() == null
-                    || ((ExpiredObject) this).getExpiredTime().after(new Date()), "object expired");
+        if (this instanceof ExpiredObject
+                && ((ExpiredObject) this).getExpiredTime() != null
+                && ((ExpiredObject) this).getExpiredTime().before(new Date())) {
+
+            return "object expired";
         }
 
-        return (E) this;
+        return null;
     }
 
 }
