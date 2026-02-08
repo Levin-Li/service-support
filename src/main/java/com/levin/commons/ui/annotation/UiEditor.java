166a6d1bccab000ab8922a14d83c2810b19f6ce1
lilw <vince2li@gmail.com>
2026-02-08 23:18:08 +0800

    优化新版rbac

--- src/main/java/com/levin/commons/ui/annotation/UiEditor.java
+++ src/main/java/com/levin/commons/ui/annotation/UiEditor.java
@@ -1,9 +1,10 @@
 package com.levin.commons.ui.annotation;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.lang.annotation.*;
 
 /**
- *
  * @author llw
  */
 @Target({ElementType.FIELD})
@@ -13,37 +14,42 @@ import java.lang.annotation.*;
 public @interface UiEditor {
 
     /**
-     *
      * 表单定义, 表单url 或是表单id
      * url: http://xxx.xxx.xxx/xxx.json
      * id: xx
-     *
+     * <p>
      * 无默认值
-     *
+     * <p>
      * 表单优先级高于jsonSchema
      *
      * @return
      */
+    @Schema(description = "表单定义, 表单url 或是表单id,     * class: xx.xx.xx\n" +
+            "     * attrName: 当前类的属性名\n" +
+            "     * id: xx\n" +
+            "     * 默认当前字段的类型")
     String form() default "";
 
     /**
-     *
      * class: xx.xx.xx
      * attrName: 当前类的属性名
-     *
+     * id: xx
      * 默认当前字段的类型
      *
      * @return
      */
+    @Schema(description = "     * class: xx.xx.xx\n" +
+            "     * attrName: 当前类的属性名\n" +
+            "     * id: xx\n" +
+            "     * 默认当前字段的类型")
     String jsonSchema() default "";
 
     /**
-     *
      * 扩展信息
      *
      * @return
      */
-    String  exInfo() default "";
+    String exInfo() default "";
 
     /**
      * 动作描述
