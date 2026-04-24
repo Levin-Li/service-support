d68a04d13c76cb380be8f3281417b889d2b674a1
lilw <vince2li@gmail.com>
2026-04-24 15:04:35 +0800

    支持Spring boot 4

--- src/main/java/com/levin/commons/rbac/RbacBaseAuthorizeService.java
+++ src/main/java/com/levin/commons/rbac/RbacBaseAuthorizeService.java
@@ -27,7 +27,7 @@ public interface RbacBaseAuthorizeService {
     static ResConditionAction newResConditionAction(@NotNull ResAuthorize resAuthorize) {
         return new ResConditionActionObject()
                 .action(resAuthorize.action())
-                .actionTypes(resAuthorize.actionTypes())
+
                 .isAndMode(resAuthorize.isAndMode())
                 .anyUserTypes((resAuthorize.anyUserTypes()))
                 .anyRoles(resAuthorize.anyRoles())
@@ -53,7 +53,7 @@ public interface RbacBaseAuthorizeService {
                 .type(resType)
                 .res(res)
                 .action(conditionAction.action())
-                .actionTypes(conditionAction.actionTypes())
+
                 .isAndMode(conditionAction.isAndMode())
                 .anyUserTypes(conditionAction.anyUserTypes())
                 .anyRoles(conditionAction.anyRoles())
